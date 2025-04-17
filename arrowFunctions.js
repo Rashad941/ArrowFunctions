13a56a8754c5930652207c4b7669c24c25ddbed0
Michael Ethan Rashad-Nakpan <methan941@gmail.com>
2025-04-17 22:03:31 +0000

    Add files via upload

---
+++ arrowFunctions.js
@@ -0,0 +1,20 @@
+//Test 1
+const age = 15;
+const firstArr = (age) => {
+    if (age === 15) {
+        console.log ('True');
+
+    } else {
+        console.log ('False')
+    }
+ };
+ firstArr(15);
+
+
+//Test 2
+const myself = (name, age) => {
+    console.log(`My name is ${name}. I am ${age} years old.`)
+};
+myself ('Michael Ethan', 40);
+
+
